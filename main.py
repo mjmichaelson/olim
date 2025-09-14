572d3fa68ece20a4d3c7a78a1eb818154800ce82
mjmichaelson <11299612+mjmichaelson@users.noreply.github.com>
2025-09-14 00:42:24 -0400

    new fast version of embedding builder

--- main.py
+++ main.py
@@ -21,20 +21,21 @@
 This creates a serialized file of spaCy 'doc' objects for each line of the search_corpus, saving it to disk.
 Only a tok2vec pipe is used because the only purpose of this file will be to support similarity calculations.
 
+* num_corpus_lines: the number of lines of the corpus to use, if -1 the whole corpus is used
+* n_process: number of procs to parallelize the conversion
+* save: whether to save to disk or return result directly
 * output_dir: the dir to save in
-* num_corpus_lines: the number of lines of the corpus to use, if null the whole corpus is used
-* parallel: whether or not to use parallel processing -- generally for anything larger than 10,000 lines it probably makes sense
-* overwrite: what to do if file exists
+* overwrite: what to do if saving and file exists
 
 '''
-def build_and_save_embeddings_to_file(output_dir='./embeddings', num_corpus_lines=10000, parallel=True, overwrite=False):
+def build_embeddings(num_corpus_lines=10000, n_process=1, save=True, output_dir='./embeddings', overwrite=False):
     import spacy
     import json
     import time
     import os
 
     if os.path.exists(output_dir):
-        if not overwrite:
+        if save and not overwrite:
             print(f'Not building and saving embeddings. File at output dir already exists and \'overwrite\' is set to False')
             return
 
@@ -49,38 +50,45 @@ def build_and_save_embeddings_to_file(output_dir='./embeddings', num_corpus_line
         full_latin_corpus = json.load(f)
     print(f'Full corpus loaded as JSON -- train + test is {len(full_latin_corpus)} lines long')
 
-    
     # convert corpus data to proper format
-    print(f'Converting {num_corpus_lines} lines to SpaCy doc format (calculating embeddings)...')
+    l = 'all' if num_corpus_lines == -1 else num_corpus_lines
+    print(f'Converting {l} lines to SpaCy doc format (calculating embeddings)...')
     start_time = time.perf_counter()
-    text = full_latin_corpus['train'][:num_corpus_lines]
-    # parallelizable step:
-    if parallel:
-        text_doc = nlp.pipe(text, n_process=4)
+    if num_corpus_lines==-1:
+        text = full_latin_corpus['train']       
     else:
-        text_doc = [nlp.make_doc(chunk) for chunk in text]
+        text = full_latin_corpus['train'][:num_corpus_lines]
+    # parallelizable step:
+    if n_process==1:
+        text_doc = [nlp.make_doc(chunk) for chunk in text]        
+    else:
+        text_doc = nlp.pipe(text, n_process=n_process)
     end_time = time.perf_counter()
     print(f'Conversion took {end_time - start_time} seconds')
 
-    # collect the docs into one object for the write
-    print(f'Collecting {num_corpus_lines} Docs into one DocBin...')
-    print(f'/-',end='')
-    start_time = time.perf_counter()
-    doc_bin = spacy.tokens.DocBin()
-    i=0
-    for d in text_doc:
-        doc_bin.add(d)
-        if i % (num_corpus_lines//10) == 0:
-            print('-',end='')
-        i += 1
-    end_time = time.perf_counter()
-    print(f'/')
-    print(f'Collection took {end_time - start_time} seconds')
+    if save:
+        # collect the docs into one object for the write
+        print(f'Collecting {num_corpus_lines} Docs into one DocBin...')
+        print(f'/-',end='')
+        start_time = time.perf_counter()
+        doc_bin = spacy.tokens.DocBin()
+        i=0
+        for d in text_doc:
+            doc_bin.add(d)
+            if i % (num_corpus_lines//10) == 0:
+                print('-',end='')
+            i += 1
+        end_time = time.perf_counter()
+        print(f'/')
+        print(f'Collection took {end_time - start_time} seconds')
 
-    # Save the doc objects to the specified directory
-    print(f'Writing DocBin to disk...')
-    doc_bin.to_disk(output_dir)
-    print(f"File of docs saved to: {output_dir}")
+        # Save the doc objects to the specified directory
+        print(f'Writing DocBin to disk...')
+        doc_bin.to_disk(output_dir)
+        print(f"File of docs saved to: {output_dir}")
+    else:
+        return text_doc
+
 
 def load_embedding_file(file_name):
     import spacy
@@ -92,33 +100,54 @@ def load_embedding_file(file_name):
     vocab = nlp.vocab
 
     # Deserialize the DocBin
-    doc_bin = spacy.tokens.DocBin().from_file(file_name)
+    print('Deserializing saved file...')
+    doc_bin = spacy.tokens.DocBin().from_disk(file_name)
 
     # Get the individual Doc objects
     return list(doc_bin.get_docs(vocab))
 
-def language_system_setup():
+
+def calculate_similarity_scores_by_chunk(user_prompt_text, corpus_embeddings_chunk, nlp=None):
     # import dependencies
     import spacy
     
-    # load latincy model into memory
-    nlp = spacy.load("la_core_web_lg")
+    if not nlp:
+        # load latincy model into memory
+        nlp = spacy.load("la_core_web_lg")
+    
+    # get embedding for user prompt
+    user_prompt_text_doc = nlp.make_doc(user_prompt_text)
 
-    # download text corpus
+    # iterate over everything in the chunk to get similarities
+    res_chunk = []
+    for i, doc_i in enumerate(corpus_embeddings_chunk):
+        res_chunk.append([i, doc_i.similarity(user_prompt_text_doc)])
 
+    return res_chunk
 
 
 def main():
-    language_system_setup()
+    #language_system_setup()
+    pass
+
 
 if __name__ == '__main__':
     #main()
 
-    build_and_save_embeddings_to_file(output_dir='./embeddings', num_corpus_lines=100000, parallel=True, overwrite=True)
+    import time
+    import spacy
 
+    output_dir = './embeddings'
+
+    text_doc = build_embeddings(num_corpus_lines=-1, n_process=4, save=False, output_dir=output_dir, overwrite=False)
+
+    #corpus_embeddings = load_embedding_file(output_dir)
+
+    #print(corpus_embeddings[:10])
+    
     # get user target_phrase
-    #target_phrase = 'sum magister' #'Urbis in Monte Tarpeio'
-    #target_doc = nlp.make_doc(target_phrase)
+    # target_phrase = 'sum magister' #'Urbis in Monte Tarpeio'
+    # target_doc = nlp.make_doc(target_phrase)
 
     # # calculate similarity scores
     # print(f'Calculating similarity scores...')
